5e16514612cb3273f9d422f8d587d8578136391e
Your Name <zak.aitmhand@gmail.com>
2020-08-11 17:47:52 -0400

    initial commit with components folder

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,7 +4,7 @@ import {MatCardModule} from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './components/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
