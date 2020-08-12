795dfc39338d5f48bc384e4010bcaf3aa3b3d79e
NoahWhite1 <41762591+NoahWhite1@users.noreply.github.com>
2020-08-12 17:15:32 -0400

    Finshed button inputs for everything but search

--- frontend/src/app/components/home-page/home-page.component.ts
+++ frontend/src/app/components/home-page/home-page.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-home-page',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
+  @Input() minValue:number;
+  @Input() maxValue:number;
+  @Input() searchValue;
+
+
   constructor() { }
+
 
   ngOnInit(): void {
   }
 
+
+  async search() {
+
+    alert("search for " +  this.searchValue);
+    this.searchValue = "";
+  }
+
+  lowToHighCost(){
+    alert("low to high button");
+  }
+
+  highToLowCost(){
+    alert("high to low button");
+  }
+
+  costBetween(){
+    alert("cost betweeen: " + this.minValue + " and " + this.maxValue);
+  }
 }
