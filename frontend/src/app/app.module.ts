68ce52460e8060c9a426694dfe62042d92b39dc8
Your Name <zak.aitmhand@gmail.com>
2020-08-13 13:03:42 -0400

    getting rid of older files

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { FormsModule } from '@angular/forms';
-import { HomePageComponent } from './components/home-page/home-page.component';
-
 
 
 @NgModule({
